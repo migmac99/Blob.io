bc5ad5cba7fca604c4d7fde52f6493ada9815661
migmac99 <migmacthegamer@gmail.com>
2018-10-01 23:34:03 +0100

    3 players :P

--- src/sketch.js
+++ src/sketch.js
@@ -6,20 +6,25 @@ const lineColor = '#8bf6ff'; //Pink
 const enemyBallColor = '#b4004e'; //Dark Pink
 const enemyLineColor = '#ff94c2'; //Pink
 
+const enemy2BallColor = '#f57f17'; //Orange
+const enemy2LineColor = '#ffee58'; //Yellow
+
 var blob;
-var enemy;
+var enemy1;
+var enemy2;
 
 var distance = 10; // Distance between both balls
 var speed = 5; // Speed of rotation
 
 
 function setup() {
-    createCanvas(600, 600); // Canvas Size
+    createCanvas(960, 540); // Canvas Size
     angleMode(DEGREES);
 
     // Spawning the blobs 
     blob1 = new Blob(ballColor, lineColor);
-    enemy = new Blob(enemyBallColor, enemyLineColor);
+    enemy1 = new Blob(enemyBallColor, enemyLineColor);
+    enemy2 = new Blob(enemy2BallColor, enemy2LineColor);
 }
 
 function draw() {
@@ -27,15 +32,19 @@ function draw() {
     blob1.update();
     blob1.show();
 
-    enemy.update();
-    enemy.show();
+    enemy1.update();
+    enemy1.show();
+
+    enemy2.update();
+    enemy2.show();
 }
 
 function keyPressed() {
     if (keyCode == SPACE) {
         blob1.switch();
     } else if (keyCode == UP_ARROW) {
-        enemy.switch();
+        enemy1.switch();
+    } else if (keyCode == DOWN_ARROW) {
+        enemy2.switch();
     }
-
 }
